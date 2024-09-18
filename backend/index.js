51b939e2d53e5c6fe4814bedfba610677d11847e
binaryboom <raghavkatariaaa@gmail.com>
2024-09-19 01:04:04 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,13 @@ import userRoute from './routes/user.route.js'
 import postRoute from './routes/post.route.js'
 import messageRoute from './routes/message.route.js'
 import { app,server } from './socket/socket.js'
+import path from 'path'
 
 dotenv.config({})
 
 const PORT=process.env.PORT || 8000;
+
+const __dirname=path.resolve()
 
 
 // const app = express()
@@ -30,6 +33,10 @@ app.use('/api/v1/user',userRoute)
 app.use('/api/v1/post',postRoute)
 app.use('/api/v1/message',messageRoute)
 
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,'0.0.0.0',()=>{
     connectDB()
